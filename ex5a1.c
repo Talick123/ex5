cd9938c15bb4639331da1f298abfcafbc0c58ec0
Tali Kalev <talkalev@gmail.com>
2021-12-08 18:18:40 +0200

    finised writing, waiting for first compile

--- ex5a1.c
+++ ex5a1.c
@@ -14,3 +14,133 @@
  ftok(".", '5')
 
 */
+
+// --------include section------------------------
+
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h> //for pause
+#include <errno.h>
+#include <stdlib.h>
+
+#define ARR_SIZE 1000
+#define LOCK 4
+
+// --------const and enum section------------------------
+
+enum STATE {LOCKED, UNLOCKED};
+const int MAX_NUM = 1000;
+
+// --------prototype section------------------------
+
+void catch_sig1(int signum);
+void create_shared_mem(key_t *key, int *shm_id, int *shm_ptr);
+void init_data(int *shm_ptr);
+void read_and_print_data(int *shm_ptr);
+void reset_arr(int arr[]);
+void close_shared_mem(int *shm_id, struct shmid_ds *shm_desc);
+void perrorandexit(char *msg);
+
+// --------main section------------------------
+
+int main()
+{
+    key_t key;
+    int shm_id;
+    int *shm_ptr;
+    struct shmid_ds shm_desc;
+
+    signal(SIGUSR1, catch_sig1);
+
+    create_shared_mem(&key, &shm_id, shm_ptr);
+    init_data(shm_ptr);
+    sleep();
+    read_and_print_data(shm_ptr);
+    close_shared_mem(&shm_id, &shm_desc);
+
+    return EXIT_SUCCESS;
+}
+
+//-------------------------------------------------
+
+void catch_sig1(int signum){}
+
+//-------------------------------------------------
+
+void create_shared_mem(key_t *key, int *shm_id,int *shm_ptr)
+{
+    *key = ftok(".", '5');
+    if(*key == -1)
+        perrorandexit("ftok failed");
+
+    if((*shm_id = shmget(key, ARR_SIZE, IPC_CREAT | 0600)) == -1)
+        perrorandexit("shmget failed");
+
+    shm_ptr = (int*)shmat(smh_id, NULL, 0);
+    if (!shm_ptr)
+        perrorandexit("shmat failed");
+}
+
+//-------------------------------------------------
+
+void init_data(int *shm_ptr)
+{
+    int index;
+
+    shm_ptr[0] = getpid();
+
+    for(index = 1; index < ARR_SIZE, index++)
+        shm_ptr[index] = 0;
+
+    shm_ptr[LOCK] = UNLOCKED;
+}
+
+//-------------------------------------------------
+
+void read_and_print_data(int *shm_ptr)
+{
+    int index, counter = 0, max = 0, min = MAX_NUM;
+    int arr[ARR_SIZE];
+    reset_arr(arr);
+
+    for(index = 0; index < ARR_SIZE; index++)
+    {
+        arr[index]++;
+        if(shm_ptr[index] < min)
+            min = shm_ptr[index];
+        if(shm_ptr[index] > max)
+            max = shm_ptr[index];
+    }
+
+    for(index = 0; index < ARR_SIZE; index++)
+        if(arr[index] != 0)
+            counter++;
+
+    printf("The number of different primes received is: %d\n", counter);
+	printf("The max prime is: %d. The min prime is: %d\n", max, min);
+}
+
+//-------------------------------------------------
+
+void reset_arr(int arr[])
+{
+	int index;
+	for(index = 0; index < ARR_SIZE; index++)
+		arr[index] = 0;
+}
+
+//-------------------------------------------------
+
+void close_shared_mem(int *shm_id, struct shmid_ds *shm_desc)
+{
+    if(shmctl(shm_id, IPC_RMID, shm_desc) == -1)
+        perrorandexit("shmctl failed");
+}
+
+//-------------------------------------------------
+
+void perrorandexit(char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
--- ex5a2.c
+++ ex5a2.c
@@ -21,3 +21,182 @@ Note: when 1 child sees full, let other children see too and send signal,
 
 
 */
+
+
+
+// --------include section------------------------
+
+
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h> //for pause
+#include <errno.h>
+#include <stdlib.h>
+
+#define ARR_SIZE 1000
+#define LOCK 4
+
+// --------const and enum section------------------------
+
+const int ARGC_SIZE = 2;
+enum STATE {LOCKED, UNLOCKED};
+
+// --------prototype section------------------------
+
+void check_argv(int argc);
+void connect_to_shared_mem(key_t *key, int *shm_id, int *shm_ptr);
+void init_and_wait(int *shm_ptr, int id);
+void fill_arr(int *shm_ptr, int arr[], int *count);
+void reset_arr(int arr[]);
+bool prime(int num);
+void print_data(int arr[], int count);
+void perrorandexit(char *msg);
+
+// --------main section------------------------
+
+int main(int argc, char *argv[])
+{
+    key_t key;
+    int shm_id;
+    int *shm_ptr;
+    int arr[ARR_SIZE], count = 0;
+
+    check_argv(argc);
+    seed(atoi(argv[1]));
+    connect_to_shared_mem(&key, &shm_id, shm_ptr);
+    init_and_wait(shm_ptr, atoi(argv[1]));
+    fill_arr(shm_ptr, arr, &count);
+    print_data(arr, count);
+
+
+    return EXIT_SUCCESS;
+}
+
+//-------------------------------------------------
+
+void check_argv(int argc)
+{
+	if(argc != ARGC_SIZE)
+		perror_and_exit("Error! Incorrect number of arguments");
+}
+
+//-------------------------------------------------
+
+void connect_to_shared_mem(key_t *key, int *shm_id, int *shm_ptr);
+{
+    key = ftok(".", '5');
+    if(key == -1)
+        perrorandexit("ftok failed");
+
+    if((shm_id = shmget(key, ARR_SIZE, 0600)) == -1)
+        perrorandexit("shmget failed");
+
+    shm_ptr = (int*)shmat(smh_id, NULL, 0);
+    if (!shm_ptr)
+        perrorandexit("shmat failed");
+}
+
+//-------------------------------------------------
+
+void init_and_wait(int *shm_ptr, int id);
+{
+    shm_ptr[id] = 1;
+
+    while(shm_ptr[1] = 0 || shm_ptr[2] = 0 || shm_ptr[3] = 0)
+        sleep(1);
+}
+
+//-------------------------------------------------
+
+void fill_arr(int *shm_ptr, int arr[], int *count)
+{
+    int num, index = 5;
+
+    reset_arr(primes_count);
+
+    while(true)
+    {
+        num = rand()%(ARR_SIZE -1) + 2;
+
+        if(prime(num))
+        {
+            while(shm_ptr[LOCK] == LOCKED)
+                sleep(1);
+
+            shm_ptr[LOCK] = LOCKED;
+
+            while(shm_ptr[index] != 0)
+                index++;
+
+            if(index >= ARR_SIZE)
+            {
+                kill(shm_ptr[0], SIGUSR1);
+                shm_ptr[LOCK] = 1; //gives other processes chance to see its full
+                return;
+            }
+
+            shm_ptr[index] = num;
+            arr[num]++;
+
+            if(new_prime(shm_ptr, index))
+                *count++;
+
+            shm_ptr[LOCK] = UNLOCKED;
+        }
+    }
+}
+
+//-------------------------------------------------
+
+void reset_arr(int arr[])
+{
+	int index;
+	for(index = 0; index < ARR_SIZE; index++)
+		arr[index] = 0;
+}
+
+//-------------------------------------------------
+
+bool prime(int num)
+{
+	int i;
+	for(i = 2; i*i <= num; i++)
+		if(num % i == 0)
+			return false;
+
+	return true;
+}
+
+//-------------------------------------------------
+
+bool new_prime(int *shm_ptr, int curr_ind)
+{
+    int index;
+
+    for(index = 5; index < curr_ind; index++)
+        if(shm_ptr[index] == shm_ptr[curr_ind])
+            return false;
+
+    return true;
+}
+
+
+void print_data(int arr[], int count)
+{
+    int max_count = 0, prime, index;
+
+    for(index = 0; index < 0; index++)
+        if(arr[index] > max_count)
+            prime = index;
+
+    printf("Process %d sent % different new primes. The prime it sent most was %d, %d times ",
+    	   (int)getpid(), count, prime, max_count);
+}
+
+//-------------------------------------------------
+
+void perrorandexit(char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
